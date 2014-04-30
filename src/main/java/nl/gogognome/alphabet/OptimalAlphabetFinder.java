1fad61d663f5e3dca208e8945c05c96c5e7e8a80
Sander Kooijmans <sander@gogognome.nl>
2014-04-30 21:16:07 +0200

    Store all candidates and counts in a list. Only first element of sorted list is used for now. This is a preparation for recursion.

--- src/main/java/nl/gogognome/alphabet/OptimalAlphabetFinder.java
+++ src/main/java/nl/gogognome/alphabet/OptimalAlphabetFinder.java
@@ -1,32 +1,45 @@
 package nl.gogognome.alphabet;
 
+import static com.google.common.collect.Lists.*;
+
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class OptimalAlphabetFinder {
 
 	public Alphabet findOptimalAlphabet(List<String> words) {
 		char[] usedCharacters = determineCharactersUsedInWords(words);
+		shuffleCharacters(usedCharacters);
 
 		Alphabet alphabet = new Alphabet("");
 		while (alphabet.toString().length() < usedCharacters.length) {
-			Alphabet bestAlphabet = null;
-			int bestNrWordsInOrder = -1;
+			List<ObjectWithCount<Alphabet>> alphabetsWithCounts = newArrayList();
 			for (char letter : usedCharacters) {
 				if (alphabet.containsLetter(letter))
 					continue;
 
 				for (Alphabet candidateAlphabet : alphabet.insertLetterAtAllPositions(letter)) {
 					int nrWordsInOrder = candidateAlphabet.countNrWordsInOrder(words);
-					if (nrWordsInOrder > bestNrWordsInOrder) {
-						bestAlphabet = candidateAlphabet;
-						bestNrWordsInOrder = nrWordsInOrder;
-					}
+					alphabetsWithCounts.add(new ObjectWithCount<Alphabet>(candidateAlphabet, nrWordsInOrder));
 				}
 			}
-			alphabet = bestAlphabet;
-			System.out.println("alphabet so far: " + alphabet + " (" + bestNrWordsInOrder + " words in order)");
+			Collections.sort(alphabetsWithCounts);
+			alphabet = alphabetsWithCounts.get(0).getObject();
+			System.out.println("alphabet so far: " + alphabet + " (" + alphabetsWithCounts.get(0).getCount() + " words in order)");
 		}
 		return alphabet;
+	}
+
+	private void shuffleCharacters(char[] usedCharacters) {
+		Random random = new Random();
+		for (int i = 0; i < 100; i++) {
+			int index1 = random.nextInt(usedCharacters.length);
+			int index2 = random.nextInt(usedCharacters.length);
+			char temp = usedCharacters[index1];
+			usedCharacters[index1] = usedCharacters[index2];
+			usedCharacters[index2] = temp;
+		}
 	}
 
 	private char[] determineCharactersUsedInWords(List<String> words) {
@@ -47,5 +60,33 @@ public class OptimalAlphabetFinder {
 		char[] trimmedUsedCharacters = new char[nrLettersPresent];
 		System.arraycopy(usedCharacters, 0, trimmedUsedCharacters, 0, nrLettersPresent);
 		return trimmedUsedCharacters;
+	}
+
+	private static class ObjectWithCount<T> implements Comparable<ObjectWithCount> {
+		private final T object;
+		private final int count;
+
+		public ObjectWithCount(T object, int count) {
+			this.object = object;
+			this.count = count;
+		}
+
+		public T getObject() {
+			return object;
+		}
+
+		public int getCount() {
+			return count;
+		}
+
+		@Override
+		public int compareTo(ObjectWithCount that) {
+			return that.count - this.count; // sorts on count descendingly
+		}
+
+		@Override
+		public String toString() {
+			return count + "x" + object;
+		}
 	}
 }
